5ad3aee9a912310cf32600da96cc74ef472dcdde
matheusfmc-digitalcollege <matheusfmc2005+digitalcollege@gmail.com>
2025-06-07 16:38:05 -0300

    inicialização do Projeto SEVEN

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,10 @@
-import { Button } from "antd";
-import { useContext } from "react";
-import { LoginContex } from "../contexts/LoginContext";
+import Logo from "../components/Logo_Seven.png";
 import "./Header.css";
-import Logo from "../components/Logo_DTI.jpg";
 
-const Header = () => {
-    const { usuario, setUsuario } = useContext(LoginContex);
-
-    return (
-        <header className="header-container">
-            <img src={Logo} alt="Logo DTI" className="header-logo" />
-            
-            <div className="user-actions">
-                {usuario ? (
-                    <div className="user-info">
-                        <span className="user-name">{usuario.NM_USUARIO}</span>
-                        <button className="logout-btn" onClick={() => {
-                            setUsuario(null);
-                            sessionStorage.removeItem("usuario");
-                        }}>
-                            Sair
-                        </button>
-                    </div>
-                ) : (
-                    <Button type="primary" className="login-btn" href="/login">
-                        Detalhes/Extrato
-                    </Button>
-                )}
-            </div>
-        </header>
-    );
-};
+const Header = () => (
+    <header className="header-container">
+        <img src={Logo} alt="Logo DTI" className="header-logo" />
+    </header>
+);
 
 export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ConfigProvider } from 'antd'
+import App from './App'
+import 'antd/dist/reset.css' // ou 'antd/dist/antd.css', conforme seu setup
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const container = document.getElementById('root')
+if (!container) {
+  throw new Error('Root element not found')
+}
+
+const root = ReactDOM.createRoot(container)
+root.render(
+  <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        cssVar: true,   // ativa CSS Variables
+        hashed: false   // desliga as hashes das classes
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>
 )
